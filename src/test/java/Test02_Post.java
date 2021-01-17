dd370f624319c5de48b1bc72821d595539012be8
Arif <arif434@gmail.com>
2021-01-16 20:16:38 -0800

    first commit

---
+++ src/test/java/Test02_Post.java
@@ -0,0 +1,26 @@
+import static io.restassured.RestAssured.given;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+public class Test02_Post {
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void test2() {
+
+		JSONObject request = new JSONObject();
+		request.put("name", "chaya");
+		request.put("job", "BA");
+
+		System.out.println(request);
+		System.out.println(request.toString());
+
+		given().
+		body(request.toJSONString()).
+		when().
+		post("https://reqres.in/api/users").
+		then().statusCode(201);
+
+	}
+
+}
